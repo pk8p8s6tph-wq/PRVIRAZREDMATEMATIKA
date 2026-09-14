c026a9895cce72c3ca0b4f990206dadad35e3cf6
pk8p8s6tph-wq <pk8p8s6tph@privaterelay.appleid.com>
2026-09-14 12:15:55 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='dino-v3';
-const FILES=['./','./index.html','./sabiranje.html','./oduzimanje.html','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
